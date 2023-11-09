3c4bbe5751237aebbcd777e6873e6094cc6c5328
Drew Swanner <drew@drewswanner.dev>
2023-11-08 18:55:04 -0600

    Add Angular Material Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-            CommonModule, 
-            MatSlideToggleModule,
-            RouterOutlet
-          ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prefered-angular-setup';
+
 }
